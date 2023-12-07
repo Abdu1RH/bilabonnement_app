8ec71628a952b96d355f880b01a129b8fa6fcda1
Amalie956 <amal956e@stud.kea.dk>
2023-12-07 21:17:50 +0100

    Container

--- bilabonnementapp/src/components/Subscription/AvailableCarList.js
+++ bilabonnementapp/src/components/Subscription/AvailableCarList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-{/*import './AvailableCar.css';*/}
 
 function AvailableCar() {
   const carBrands = [
@@ -22,16 +21,25 @@ function AvailableCar() {
   ];
 
   return (
-    <div>
-      <h1>Ledige biler</h1>
-      <ul>
-        {carBrands.map((brand, index) => (
-          <li key={index}>
-            {/* Link til CreateSubscription med bilens navn som parameter */}
-            <Link to={`/createSubscription/${brand}`}>{brand}</Link>
-          </li>
-        ))}
-      </ul>
+    <div className="container-gray">
+      {/* Container til "Ledige aftaler" - grå baggrund */}
+      <div className="inner-container">
+        <h1>Ledige aftaler</h1>
+      </div>
+
+      {/* Container til listen af biler - hvid baggrund */}
+      <div className="outer-container">
+        <div className="car-container">
+          <ul className="car-list">
+            {carBrands.map((brand, index) => (
+              <li className="car-list-item" key={index}>
+                {/* Link til CreateSubscription med bilens navn som parameter */}
+                <Link to={`/createSubscription/${brand}`}>{brand}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
