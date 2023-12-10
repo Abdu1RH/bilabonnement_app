6f65f08275ef5f94ac799e52cff2b50e6cf51047
Abdu1RH <abdul.rh2403@gmail.com>
2023-12-10 18:02:49 +0100

    #20 Ikke gjort endnu ændret små detaljer

--- bilabonnementapp/src/App.js
+++ bilabonnementapp/src/App.js
@@ -44,7 +44,7 @@ function App() {
                 to="/ListOfAvailableCars"
                 className="btn btn-info btn-overlay"
               >
-                Opret lejeaftale
+                Administrer lejeaftale
               </Link>
             </div>
             {/* Mellemrum mellem containere */}
@@ -58,7 +58,7 @@ function App() {
                 className="image-size"
               />
               <Link to="/createDamageReport" className="btn btn-info">
-                Opret skaderapport
+                Administrer skaderapport
               </Link>
             </div>
 
