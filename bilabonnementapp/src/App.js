908a46b9b47a11a750cd07749afe00dc00d43b3a
Abdu1RH <abdul.rh2403@gmail.com>
2023-12-11 19:02:25 +0100

    #26 implementeret subscription i damageReport

--- bilabonnementapp/src/App.js
+++ bilabonnementapp/src/App.js
@@ -21,7 +21,7 @@ function App() {
           <div className="container-fluid">
             <div className="navbar-header">
               <a className="navbar-brand" href="#">
-                Bilabonnement intern
+                Bilabonnement intern system
               </a>
             </div>
             {/* Din øverste menu */}
