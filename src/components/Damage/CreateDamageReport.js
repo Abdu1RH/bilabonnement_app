8a42e1ad51c2050f854cef6c7cc64c3621b7f7b4
Mahe0055 <mahe00555@stud.kea.dk>
2023-12-13 13:03:01 +0100

    #37 damagereport design side

--- src/components/Damage/CreateDamageReport.js
+++ src/components/Damage/CreateDamageReport.js
@@ -1,91 +1,96 @@
 // CreateDamageReport.js
 import React, { useState } from "react";
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import Subscription from "../Subscription/Subscription";
 
 function CreateDamageReport() {
-    const navigate = useNavigate();
-    const [newDamageReport, setNewDamageReport] = useState({
-        error: "",
-        errorType: "",
-        numberOfErrors: 0,
-        pricePerError: 0.0,
-    });
+  const navigate = useNavigate();
+  const [newDamageReport, setNewDamageReport] = useState({
+    error: "",
+    errorType: "",
+    numberOfErrors: 0,
+    pricePerError: 0.0,
+  });
 
-    const handleCreate = (e) => {
-        e.preventDefault();
-        axios.post("http://localhost:8080/api/damagereports", newDamageReport)
-            .then(() => navigate("/"))
-            .catch(error => console.error("Error creating damage report", error));
-    };
+  const handleCreate = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8080/api/damagereports", newDamageReport)
+      .then(() => navigate("/"))
+      .catch((error) => console.error("Error creating damage report", error));
+  };
 
-    const handleChange = (e) => {
-        const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
-        setNewDamageReport({ ...newDamageReport, [e.target.name]: value });
-    };
+  const handleChange = (e) => {
+    const value =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    setNewDamageReport({ ...newDamageReport, [e.target.name]: value });
+  };
 
-    return (
-        <div className="container-gray">
-            <div className="inner-container">
-                <h1>Skade og udbedring</h1>
+  return (
+    <div className="container-gray">
+      <h1 className="damage-headline">Skade og udbedring</h1>
+      <Subscription
+        sendToParent={newDamageReport}
+        handleChange={handleChange}
+      />
+      <div className="outer-container">
+        <div className="damage-containers">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="subscription-container">
+                <form className="form-damage" onSubmit={handleCreate}>
+                  <div>
+                    <label>Skade</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="error"
+                      value={newDamageReport.error}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div>
+                    <label>Type af skade</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="errorType"
+                      value={newDamageReport.errorType}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div>
+                    <label>Antal skader</label>
+                    <input
+                      type="number"
+                      className="form-control"
+                      name="numberOfErrors"
+                      value={newDamageReport.numberOfErrors}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div>
+                    <label>Pris per skade</label>
+                    <input
+                      type="number"
+                      className="form-control"
+                      name="pricePerError"
+                      value={newDamageReport.pricePerError}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </form>
+                <button className="createDamageReport-btn" type="submit">
+                  Opret skade raport
+                </button>
+              </div>
             </div>
-            <Subscription sendToParent={newDamageReport} handleChange={handleChange} />
-            <div className="outer-container">
-                <div className="row">
-                    <div className="col-md-6">
-                        <div className="damage-containers">
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="subscription-container">
-                                        <form onSubmit={handleCreate}>
-                                            <div>
-                                                <label>Skade</label>
-                                                <input
-                                                    type='text'
-                                                    name='error'
-                                                    value={newDamageReport.error}
-                                                    onChange={handleChange}
-                                                />
-                                            </div>
-                                            <div>
-                                                <label>Type af skade</label>
-                                                <input
-                                                    type='text'
-                                                    name='errorType'
-                                                    value={newDamageReport.errorType}
-                                                    onChange={handleChange}
-                                                />
-                                            </div>
-                                            <div>
-                                                <label>Antal skader</label>
-                                                <input
-                                                    type='number'
-                                                    name='numberOfErrors'
-                                                    value={newDamageReport.numberOfErrors}
-                                                    onChange={handleChange}
-                                                />
-                                            </div>
-                                            <div>
-                                                <label>Pris per skade</label>
-                                                <input
-                                                    type='number'
-                                                    name='pricePerError'
-                                                    value={newDamageReport.pricePerError}
-                                                    onChange={handleChange}
-                                                />
-                                            </div>
-                                            <button type='submit'>Opret skade raport</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default CreateDamageReport;
--- src/components/Subscription/CreateSubscription.js
+++ src/components/Subscription/CreateSubscription.js
@@ -1,76 +1,118 @@
 import React, { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import Customer from "../Car/Customer";
 import Car from "../Car/Car";
 
 function CreateSubscription() {
-    const navigate = useNavigate();
-    const { id } = useParams();
-    const [newSubscription, setNewSubscription] = useState({
-        startDate: '',
-        endDate: '',
-        carID: 0,
-        customerID: 0,
-        plannedDistanceInKilometers: 0,
-    });
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [newSubscription, setNewSubscription] = useState({
+    startDate: "",
+    endDate: "",
+    carID: 0,
+    customerID: 0,
+    plannedDistanceInKilometers: 0,
+  });
 
-    const handleCreate = (e) => {
-        e.preventDefault();
-        console.log(newSubscription);
-        axios.post('http://localhost:8080/api/subscriptions', newSubscription)
-            .then(() => navigate("/"))
-            .catch(error => console.error("Error creating subscription", error));
-    };
+  const handleCreate = (e) => {
+    e.preventDefault();
+    console.log(newSubscription);
+    axios
+      .post("http://localhost:8080/api/subscriptions", newSubscription)
+      .then(() => navigate("/"))
+      .catch((error) => console.error("Error creating subscription", error));
+  };
 
-    const handleChange = (e) => {
-        const value = e.target.type === 'input' ? e.target.checked : e.target.value;
-        setNewSubscription({ ...newSubscription, [e.target.name]: value });
-    };
+  const handleChange = (e) => {
+    const value = e.target.type === "input" ? e.target.checked : e.target.value;
+    setNewSubscription({ ...newSubscription, [e.target.name]: value });
+  };
 
-    return (
-        <div className="container">
-            <div className="inner-container">
-                <h1>Lejeaftaler</h1>
-            </div>
+  return (
+    <div className="container">
+      <div className="inner-container">
+        <h1>Lejeaftaler</h1>
+      </div>
 
-            <div className="row">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="container-around-lease-container">
+            <div className="lease-container">
+              <div className="row">
                 <div className="col-md-6">
-                    <div className="container-around-lease-container">
-                        <div className="lease-container">
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="subscription-container">
-                                        <h1>Abonnement information</h1>
-                                        <Car sendToParent={newSubscription} handleChange={handleChange} />
-                                        <Customer sendToParent={newSubscription} handleChange={handleChange} />
-                                        <form onSubmit={handleCreate}>
-                                            <div className="mb-3">
-                                                <label htmlFor="startDate" className="form-label">Start Dato:</label>
-                                                <input type="date" className="form-control" id="startDate" name="startDate" value={newSubscription.startDate} onChange={handleChange} />
-                                            </div>
-                                            <div className="mb-3">
-                                                <label htmlFor="endDate" className="form-label">Slut dato:</label>
-                                                <input type="date" className="form-control" id="endDate" name="endDate" value={newSubscription.endDate} onChange={handleChange} />
-                                            </div>
-                                            <div className="mb-3">
-                                                <label htmlFor="plannedDistance" className="form-label">Antal aftalt kørt km:</label>
-                                                <input type="number" className="form-control" id="plannedDistance" name="plannedDistanceInKilometers" value={newSubscription.plannedDistanceInKilometers} onChange={handleChange} />
-                                            </div>
-                                            <button className="btn btn-primary" type="submit">Opret abonnementsaftale</button>
-                                        </form>
-                                    </div>
-                                </div>
-
-                                <Link to={`/EditSubscription/${newSubscription.id}`}> Updaterer abonnementet </Link>
-                                <Link to={`/DeleteSubscription/${newSubscription.id}`}> Slet abonnement </Link>
-                            </div>
-                        </div>
-                    </div>
+                  <div className="subscription-container">
+                    <h1>Abonnement information</h1>
+                    <Car
+                      sendToParent={newSubscription}
+                      handleChange={handleChange}
+                    />
+                    <Customer
+                      sendToParent={newSubscription}
+                      handleChange={handleChange}
+                    />
+                    <form onSubmit={handleCreate}>
+                      <div className="mb-3">
+                        <label htmlFor="startDate" className="form-label">
+                          Start Dato:
+                        </label>
+                        <input
+                          type="date"
+                          className="form-control"
+                          id="startDate"
+                          name="startDate"
+                          value={newSubscription.startDate}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="endDate" className="form-label">
+                          Slut dato:
+                        </label>
+                        <input
+                          type="date"
+                          className="form-control"
+                          id="endDate"
+                          name="endDate"
+                          value={newSubscription.endDate}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="plannedDistance" className="form-label">
+                          Antal aftalt kørt km:
+                        </label>
+                        <input
+                          type="number"
+                          className="form-control"
+                          id="plannedDistance"
+                          name="plannedDistanceInKilometers"
+                          value={newSubscription.plannedDistanceInKilometers}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <button className="btn btn-primary" type="submit">
+                        Opret abonnementsaftale
+                      </button>
+                    </form>
+                  </div>
                 </div>
+
+                <Link to={`/EditSubscription/${newSubscription.id}`}>
+                  {" "}
+                  Updaterer abonnementet{" "}
+                </Link>
+                <Link to={`/DeleteSubscription/${newSubscription.id}`}>
+                  {" "}
+                  Slet abonnement{" "}
+                </Link>
+              </div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default CreateSubscription;
